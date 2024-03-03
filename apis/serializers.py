2ca437a760c512bc4bd567567e087ebb3890fe36
AliAkbarBadri <aabadri74@gmail.com>
2024-03-03 13:47:19 +0330

    add test of api

--- apis/serializers.py
+++ apis/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 
-from books.models import Book 
+from books.models import Book
+
+
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
--- apis/views.py
+++ apis/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 from rest_framework.generics import ListAPIView
 from .serializers import BookSerializer
 from books.models import Book
+
+
 # Create your views here.
 class BookApiView(ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    
-    
